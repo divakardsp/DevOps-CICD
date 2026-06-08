c836ef1a511fbea4d04f9e78cdb1312dccb361f5
divakar singh purva <divakarpurva04@gmail.com>
2026-06-08 15:23:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({message: "Server is healthy! v1"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
