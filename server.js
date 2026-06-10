169a4a0d60cce9a32d6d8e5295e8ad72909db0bb
divakar singh purva <divakarpurva04@gmail.com>
2026-06-10 15:32:50 +0530

    Update health check message in server response

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({message: "Server is healthy! v4: Github Actions"});
+    res.json({message: "Server is healthy! v4: Github Actions: hurray!!!!"});
 })
 
 app.listen(PORT, () => {
