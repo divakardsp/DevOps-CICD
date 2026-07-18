a195ef5ca689009f4a0342a05c1a81719a9d2b22
divakar singh purva <divakarpurva04@gmail.com>
2026-07-18 22:39:09 +0530

    feat: update routes to include user and product endpoints

--- route.ts
+++ route.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { Router } from "express"
+import * as userController from "./controller/user.controller.js"
 import * as orderController from "./controller/order.controller.js"
 import * as productController from "./controller/product.controller.js"
 
 const router = Router()
 
-router.get("/order", orderController.getOrder)
-router.get("/product", productController.getProduct)
+router.get("/get-user", userController.getUser)
+router.get("/get-order", orderController.getOrder)
+router.get("/get-product", productController.getProduct)
+router.get("/get-order/:id", orderController.getOrderById)
+router.get("/get-product/:id", productController.getProductById)
