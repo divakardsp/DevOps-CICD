1a991a46e8719b639bdd14b99de0e87c8cead4d4
divakar singh purva <divakarpurva04@gmail.com>
2026-07-21 12:04:12 +0530

    feat: organize server.js by adding router and improving code formatting

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import "dotenv/config";
 import express from "express";
+import router from "./route.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(router);
+
 app.get("/", (req, res) => {
-    res.json({message: "Server is healthy! v4: Github Actions: hurray checking for PR REVIEW APP checking again UPDATED AGAIN!!!!"});
-})
+    res.json({
+        message:
+            "Server is healthy! v4: Github Actions: hurray checking for PR REVIEW APP checking again UPDATED AGAIN!!!!",
+    });
+});
 
 app.get("/health", (req, res) => {
-    res.json({healthy: true})
-    
-})
+    res.json({ healthy: true });
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
-
-
+});
