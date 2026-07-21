abb65722c38f2ad9baf8c968f3d085db9128a643
divakar singh purva <divakarpurva04@gmail.com>
2026-07-21 12:04:23 +0530

    feat: add route definitions for user, order, and product controllers

---
+++ route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as userController from "./controller/user.controller.js";
+import * as orderController from "./controller/order.controller.js";
+import * as productController from "./controller/product.controller.js";
+
+const router = Router();
+
+router.get("/get-user", userController.getUser);
+router.get("/get-user/:id", userController.getUserById);
+router.get("/get-order", orderController.getOrder);
+router.get("/get-order/:id", orderController.getOrderById);
+router.get("/get-product", productController.getProduct);
+router.get("/get-product/:id", productController.getProductById);
+
+export default router;
