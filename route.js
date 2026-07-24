9165ed30a7bffc19e13934a52f2a33b47838daf2
divakar singh purva <divakarpurva04@gmail.com>
2026-07-24 17:42:39 +0530

    feat: add CRUD routes for users, orders, and products

--- route.js
+++ route.js
@@ -7,9 +7,23 @@ const router = Router();
 
 router.get("/get-user", userController.getUser);
 router.get("/get-user/:id", userController.getUserById);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.patch("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
+
 router.get("/get-order", orderController.getOrder);
 router.get("/get-order/:id", orderController.getOrderById);
+router.post("/orders", orderController.createOrder);
+router.put("/orders/:id", orderController.updateOrder);
+router.patch("/orders/:id", orderController.updateOrder);
+router.delete("/orders/:id", orderController.deleteOrder);
+
 router.get("/get-product", productController.getProduct);
 router.get("/get-product/:id", productController.getProductById);
+router.post("/products", productController.createProduct);
+router.put("/products/:id", productController.updateProduct);
+router.patch("/products/:id", productController.updateProduct);
+router.delete("/products/:id", productController.deleteProduct);
 
 export default router;
